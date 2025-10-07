c41be000451fdb63b00a534ea319508aa1f6f587
Hazel <hasalvador@gbox.adnu.edu.ph>
2025-10-07 14:18:51 +0800

    [POST-03] - Implement service to add a post/reply

--- online_forum/post_interactions/views.py
+++ online_forum/post_interactions/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+# This view lets logged-in users create a post or reply.
+class PostCreateView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        # Automatically set "created_by" to the logged-in user
+        serializer.save(created_by=self.request.user)
