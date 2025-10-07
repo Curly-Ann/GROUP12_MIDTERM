de1ff716048c8fbe32923a92b50b00dd0f43a13d
unknown <mlumibao@gbox.adnu.edu.ph>
2025-10-07 13:10:29 +0800

    edited settings and models

--- online_forum/accounts/models.py
+++ online_forum/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+class User(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('member', 'Member'),
+    ]
 
-# Create your models here.
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='member')
+
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
+
+    def __str__(self):
+        return self.username
